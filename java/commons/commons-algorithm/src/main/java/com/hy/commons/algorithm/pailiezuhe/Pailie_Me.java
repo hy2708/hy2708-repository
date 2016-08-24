ddca74acfa6c1b461c3c1da297465e893cfcb2cf
huahongyu <huahongyu@jd.com>
2016-08-24 09:48:40 +0800

    字符串全排列

---
+++ java/commons/commons-algorithm/src/main/java/com/hy/commons/algorithm/pailiezuhe/Pailie_Me.java
@@ -0,0 +1,42 @@
+package com.hy.commons.algorithm.pailiezuhe;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Pailie_Me {
+
+	public static void main(String[] args) {
+		pailie("", "1234");
+		//for
+		System.out.println(list.size());
+
+	}
+	static List<String> list =new LinkedList<String>();
+	public static void pailie(String selected ,String rest) {
+		if (rest==null||rest.length()==0) {
+			if (selected==null||selected.length()==0) {
+				
+			}else {
+				
+				list.add(selected);
+			}
+			return;
+		}
+		for (int i = 0; i < rest.length(); i++) {
+			
+			String selectedTemp=selected+rest.charAt(i);
+			String restTemp=rest.replaceFirst(String.valueOf(rest.charAt(i)), "");
+			pailie(selectedTemp,restTemp );
+			
+			/*
+			selected=selected+rest.charAt(i);
+			rest=rest.replaceFirst(String.valueOf(rest.charAt(i)), "");
+			pailie(selected,rest );
+			 */
+		}
+	}
+	
+	
+
+}
